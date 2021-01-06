103259ccbe6cc497d27ebb0629f509b661a35911
sonidhiman <dhimansoni2000@gmail.com>
2021-01-06 13:22:25 +0530

    first commit

---
+++ src/MalformedURLException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL1
+ */
+class MalformedURLException {
+    
+    public static void main(String[] args) {
+        Integer aa = 4;
+            int bb = 4;
+            
+            System.out.println(aa == bb);
+    }
+}
+
+
---
+++ src/MediaPanel.java
@@ -0,0 +1,20 @@
+
+import java.net.URL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL1
+ */
+class MediaPanel {
+
+    MediaPanel(URL mediaURL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
